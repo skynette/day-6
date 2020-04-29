44741810413593e8b0401dfd024540cfe88df3a0
L0n3w4lf <cutejosh2@gmail.com>
2020-04-29 12:54:37 +0100

    First attempt

--- anagram.py
+++ anagram.py
@@ -1,2 +1,11 @@
 def find_anagrams(word, candidates):
-    pass
+    ans = []
+    word_length = len(word)
+    sorted_word = sorted(word.lower())
+    for words in candidates:
+        if len(words) == word_length:
+            if words.lower() == word.lower():
+                continue
+            elif sorted(words.lower()) == sorted_word:
+                ans.append(words)
+    return ans
